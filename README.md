1b13adcee90ef2533ac9816c283ea5d636ed49d6
Jeroen Peters <jeroen.peters@yourhosting.nl>
2020-08-26 15:05:51 +0200

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# bol.com-cadeaubon-restwaarde-chrome-extension
+# Bol.com Cadeaubonnen totale restwaarde
 Bekijk de totale restwaarde van al je waardebonnen op Bol.com
+
+## Gebruik
+Installeer de extensie in je browser (Chrome, Chromium, Microsoft Edge).
+Op het moment dat je op de [cadeaubonnen pagina van Bol.com](https://www.bol.com/nl/account/betaalmethoden.html#cadeaubonnen)
+komt wordt de extensie geactiveerd en voegt deze boven aan de tabel
+met je bonnen het totaal aan restwaarde toe. 
+
+## Aansprakelijkheid
+Ik ben op geen enkele manier verbonden aan Bol.com anders dan dat 
+ik klant ben en redelijk veel cadeaubonnen in mijn account heb.
+Dit is **_geen_** officiële Bol.com extensie.
+
+## Privacy
+Deze extensie leest **_alleen_** de restwaarde van nog niet (volledig) 
+gebruikte bonnen uit, en ook alleen maar op die pagina. Deze plugin
+heeft verder geen brede permissies nodig. Er worden ook geen gegevens 
+opgeslagen of doorgestuurd, alles blijft binnen je browser op die 
+ene pagina.
+
+### Sourcecode
+Om bovenstaande bewering te checken kun je `bol-restwaarde-check.js`
+inzien. Het is in Javascript geschreven.
+
+### Credits iconen
+Icons made by [monkik](https://www.flaticon.com/authors/monkik "monkik") from [www.flaticon.com](https://www.flaticon.com/ "Flaticon")
---
+++ bol-restwaarde-check.js
@@ -0,0 +1,31 @@
+function berekenRestwaarde()
+{
+    let bon_waardes = document.querySelectorAll('[data-test="giftcard-remaining-value"]');
+
+    let total = 0;
+    for (let i in bon_waardes)
+    {
+        if (bon_waardes.hasOwnProperty(i)) {
+            let bon_waarde = bon_waardes[i].innerHTML.replace("€", "").replace("&euro;", "").replace(",", ".").trim();
+            let getal_waarde = parseFloat(bon_waarde);
+            total = total + getal_waarde;
+        }
+    }
+
+    total = total/2; /* Omdat Bol ook het detailscherm al rendert in de HTML zou je de waarde dubbel krijgen.. */
+
+    const html_restwaarde = '<td data-test="giftcard-mobile-header-type" data-th="Type" colspan="3">\n' +
+        '                    <span data-test="giftcard-type">Totale restwaarde</span>\n' +
+        '                </td>\n' +
+        '                <td data-test="giftcard-mobile-header-remaining-value" data-th="Restwaarde" colspan="3">\n' +
+        '                    <span data-test="giftcard-remaining-value"><strong>&euro; ' + total.toString().replace('.', ',') + '</span>\n' +
+        '                </td>\n';
+
+    let restwaarde_rij_element = document.createElement("tr");
+    restwaarde_rij_element.innerHTML = html_restwaarde;
+
+    document.getElementById('giftcard-table-rows').insertAdjacentHTML('afterbegin', html_restwaarde);
+    document.getElementById('giftcard-table-rows').insertAdjacentHTML('beforeend', html_restwaarde);
+}
+
+window.setTimeout(berekenRestwaarde, 1000);
